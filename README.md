2a53345a94a64e2e4112cbe7d3d334ea1586b3d8
Yıldız onaran <143941553+yildiz442351@users.noreply.github.com>
2024-03-20 01:44:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Kütüphaneler
+- bootstrap
+- formik
+- yup
+- react-router-dom
+-  axios
+- chart.js
+- react-chartjs-2
+- react-icons
+- millify
+- Kaynaklar
+- API: https://docs.coincap.io/# coin-mvc-formik
---
+++ schema.js
@@ -0,0 +1,49 @@
+// yup'tdaki bütün fonksiyonları import etme
+import * as yup from 'yup';
+
+// 1 büyük harf
+// 1 küçük harf
+// 1 sayı
+// 1 özel karakter
+// min 5 karakter
+const regex =
+  '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,}$';
+
+// validasyon şeması
+// formdaki inputların geçerli olması
+// için gerekli koşulları tanımlaıdğımız yapı
+
+// bir input alnın zorunluluklarını belirrtirken
+// ilk çağırmamaız gereken fonksiyon veri tipi olmalı
+export const schema = yup.object().shape({
+  // email için zorunlulukalrı belirle
+  email: yup
+    .string()
+    .email('Geçerli bir mail formatı giriniz')
+    .required('Zorunlu alan'),
+
+  // yaş için zorunlulukları belirle
+  age: yup
+    .number()
+    .min(18, "Yaşınız 18'den küçük olamaz")
+    .max(100, "Yaşınız 100'den büyük olamaz")
+    .integer('Yaşınız tam sayı olmalı')
+    .required('Zorunlu alan'),
+
+  // şifre alanı için zorunluluklar
+  password: yup
+    .string()
+    .min(5, 'Şifre en az 5 karakter olmalı')
+    // şifre regex'deki kurallara uygun mu kontrol eder
+    .matches(regex, 'Şifreniz yeterince güçlü değil')
+    .required('Zorunlu alan'),
+
+  // şifre onay için zorunluluklar
+  confirmPassword: yup
+    .string()
+    // oneOf inputtaki veri veridiğimiz dizideki verilerden
+    // biriyle eşeleşiyormu kontrol eder
+    // yup.ref input alanlarındaki verilere erişmeye yarar
+    .oneOf([yup.ref('password')], 'Onay şifreniz doğru değil')
+    .required('Zorunlu Alan'),
+});
